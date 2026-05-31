2cb08cb02401e215eb7f03539bd746cd74643cf6
vishnu12-2005 <vishnu12-2005@users.noreply.github.com>
2026-05-31 22:57:58 +0530

    Fix Vercel deployment by moving os.environ.setdefault outside main() in manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yentroww.settings')
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yentroww.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
