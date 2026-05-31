207647f3bf51550b6652b332bc34213107008309
vishnu12-2005 <vishnu12-2005@users.noreply.github.com>
2026-05-31 22:43:59 +0530

    chore: add vercel.json configurations, requirements.txt, and update ALLOWED_HOSTS for serverless deployment

--- yentroww/settings.py
+++ yentroww/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-dummy-secret-key-yentroww'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
